1763537b812d6f3d0a70e2ac8d58839994ae51c2
Hyeonsang <250wons@gmail.com>
2021-11-23 22:09:25 +0900

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return <Container>hello</Container>;
+}
+
+const Container = styled.div`
+  background-color: steelblue;
+  color: #fff;
+`;
+
+export default App;
